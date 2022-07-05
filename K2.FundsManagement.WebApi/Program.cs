62ace4385c1eef74513c07d53695dcfd52cab242
alizaib <alizaib@gmail.com>
2022-07-05 17:21:00 +0500

    initial commit

---
+++ K2.FundsManagement.WebApi/Program.cs
@@ -0,0 +1,13 @@
+﻿using K2.FundsManagement.WebApi;
+using Karakoram.Framework.Startup;
+using Karakoram.Framework.Web;
+
+namespace K2.Accounts.WebApi;
+
+public class Program {
+    public static async Task Main(string[] args) {
+        await ServiceHost.Create<Startup>(args)
+                         .ForWeb()
+                         .RunAsync();
+    }
+}
---
+++ K2.FundsManagement.WebApi/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Karakoram.Framework.Web;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace K2.FundsManagement.WebApi; 
+
+public class Startup : WebStartup {
+    public override void ConfigureServices(IConfiguration configuration, IWebHostEnvironment hostingEnvironment, IServiceCollection services) {
+        base.ConfigureServices(configuration, hostingEnvironment, services);
+    }
+}
